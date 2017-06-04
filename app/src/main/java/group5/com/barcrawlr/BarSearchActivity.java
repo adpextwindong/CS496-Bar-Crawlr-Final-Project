0ebaa105fdb086c33a9fc4185e4dd8f330761758
George Crary <craryg@onid.oregonstate.edu>
2017-06-04 16:56:36 -0700

    Add Search Activity classes

---
+++ app/src/main/java/group5/com/barcrawlr/BarSearchActivity.java
@@ -0,0 +1,11 @@
+package group5.com.barcrawlr;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by georgecrary on 6/4/17.
+ */
+
+public class BarSearchActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/group5/com/barcrawlr/BeerSearchActivity.java
@@ -0,0 +1,11 @@
+package group5.com.barcrawlr;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by georgecrary on 6/4/17.
+ */
+
+public class BeerSearchActivity extends AppCompatActivity {
+
+}
