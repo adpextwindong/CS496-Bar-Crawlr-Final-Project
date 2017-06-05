55827987ede1f2588a4616b6a48a1e93d44c0e78
George Crary <craryg@onid.oregonstate.edu>
2017-06-04 17:33:59 -0700

    Add initial menu for favorites activities

---
+++ app/src/main/java/group5/com/barcrawlr/FavoriteBars.java
@@ -0,0 +1,17 @@
+package group5.com.barcrawlr;
+
+/**
+ * Created by georgecrary on 6/4/17.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FavoriteBars extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_beer);
+    }
+}
---
+++ app/src/main/java/group5/com/barcrawlr/FavoriteBeer.java
@@ -0,0 +1,13 @@
+package group5.com.barcrawlr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavoriteBeer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_beer);
+    }
+}
