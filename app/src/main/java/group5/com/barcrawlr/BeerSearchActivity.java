b7587f99d13f67e61cebc97a593e3cffaa13c637
omalleya <omalleya@oregonstate.edu>
2017-06-04 17:13:34 -0700

    adds use of correct layouts for search activities

--- app/src/main/java/group5/com/barcrawlr/BeerSearchActivity.java
+++ app/src/main/java/group5/com/barcrawlr/BeerSearchActivity.java
@@ -1,5 +1,6 @@
 package group5.com.barcrawlr;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,5 +8,10 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class BeerSearchActivity extends AppCompatActivity {
-
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.beer_search_activity);
+    }
 }
