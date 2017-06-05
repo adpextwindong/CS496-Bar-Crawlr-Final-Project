a26fdfde1eed61cb82a1b5a811d4fb54a9c75032
omalleya <omalleya@oregonstate.edu>
2017-06-04 17:30:30 -0700

    adds on click listener to search button in beer search activity

--- app/src/main/java/group5/com/barcrawlr/BeerSearchActivity.java
+++ app/src/main/java/group5/com/barcrawlr/BeerSearchActivity.java
@@ -2,16 +2,36 @@ package group5.com.barcrawlr;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by georgecrary on 6/4/17.
  */
 
 public class BeerSearchActivity extends AppCompatActivity {
+
+    Button mButtonSearch;
+    EditText mEditTextSearch;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.beer_search_activity);
+
+        mButtonSearch = (Button) findViewById(R.id.btn_search);
+        mEditTextSearch = (EditText) findViewById(R.id.et_search_box);
+
+        mButtonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("yo");
+                System.out.println(mEditTextSearch.getText());
+
+                //search database for value
+            }
+        });
     }
 }
