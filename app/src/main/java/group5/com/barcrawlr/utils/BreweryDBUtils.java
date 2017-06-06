1a8d7b59bc0bbb3064b9da6648a0226ac7354d87
omalleya <omalleya@oregonstate.edu>
2017-06-06 14:20:40 -0700

    starts implementation of utils and catches branch up with master

--- app/src/main/java/group5/com/barcrawlr/utils/BreweryDBUtils.java
+++ app/src/main/java/group5/com/barcrawlr/utils/BreweryDBUtils.java
@@ -5,4 +5,8 @@ package group5.com.barcrawlr.utils;
  */
 
 public class BreweryDBUtils {
+
+    public static final String baseUrl = "http://api.brewerydb.com/v2/";
+    public static final String apiKey = "00018739ed2662a0c01fb436c996e404";
+
 }
