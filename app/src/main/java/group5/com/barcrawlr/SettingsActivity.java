bfad17c43f39bc9c2b7a0d75135e51a9fc89eaff
George Crary <craryg@onid.oregonstate.edu>
2017-06-16 01:09:09 -0700

    Add Search By Zip Code and Toasts

---
+++ app/src/main/java/group5/com/barcrawlr/SettingsActivity.java
@@ -0,0 +1,18 @@
+package group5.com.barcrawlr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by georgecrary on 6/15/17.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
